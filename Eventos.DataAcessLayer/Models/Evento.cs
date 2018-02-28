c2dcde73f6bbf627f226686de6f77b7704035ca9
Andrey Sotooka <andrey.tadashi@hotmail.com>
2018-02-28 16:12:03 -0300

    Criando a Model Evento

---
+++ Eventos.DataAcessLayer/Models/Evento.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Eventos.DataAcessLayer.Models
+{
+    public class Evento
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdEvento { get; set; }
+        [Required]
+        public String Nome { get; set; }
+        public String Local { get; set; }
+        public DateTime Data { get; set; }
+        public DateTime HoraIncio { get; set; }
+        public DateTime HoraFim{ get; set; }
+        public String OpenBar { get; set; }
+        public int QuantidadeDeAmbientes { get; set; }
+        public int FaixaEtária { get; set; }
+    }
+}
