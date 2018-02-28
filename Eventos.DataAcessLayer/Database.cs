f2a9a9c639f2f257cadb602a07f2c00368304a75
Andrey Sotooka <andrey.tadashi@hotmail.com>
2018-02-28 16:25:31 -0300

    Criando a classe de conexão com banco de dados - Database

---
+++ Eventos.DataAcessLayer/Database.cs
@@ -0,0 +1,23 @@
+﻿using Eventos.DataAcessLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eventos.DataAcessLayer
+{
+    public class Database : DbContext
+    {
+        public DbSet<Evento> Eventos { get; set; }
+
+        //public BancoDeDados(DbContextOptions<BancoDeDados> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseMySql("server = localhost; " +
+            "userid=root;" +
+            "password=0000;database=eventos;");
+        }
+
+    }
+}
