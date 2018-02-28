d34601277cbd457ec0c6dabe307f873cc49530be
Andrey Sotooka <andrey.tadashi@hotmail.com>
2018-02-28 16:44:35 -0300

    Criando a ModelView de Evento

---
+++ Eventos.DataAcessLayer/ModelView/EventoModelVIew.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Eventos.DataAcessLayer.ModelView
+{
+    public class EventoModelVIew
+    {
+        [Required]
+        public String Nome { get; set; }
+        public String Local { get; set; }
+        public DateTime Data { get; set; }
+        public DateTime HoraIncio { get; set; }
+        public DateTime HoraFim { get; set; }
+        public String OpenBar { get; set; }
+        public int QuantidadeDeAmbientes { get; set; }
+        public int FaixaEtária { get; set; }
+    }
+}
